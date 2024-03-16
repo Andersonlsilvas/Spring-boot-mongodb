e481dc97d5e1012a2362d30c931a61f37355837e
Anderson Santos <andersonlsilvas86@gmail.com>
2024-03-16 20:41:10 -0300

    Obtendo um post por id

---
+++ springboot_mongodb/src/main/java/com/andersonsantos/springboot_mongodb/services/PostService.java
@@ -0,0 +1,26 @@
+package com.andersonsantos.springboot_mongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andersonsantos.springboot_mongodb.Repository.PostRepository;
+import com.andersonsantos.springboot_mongodb.entities.Post;
+import com.andersonsantos.springboot_mongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not fond"));
+		}
+
+	
+}
+
