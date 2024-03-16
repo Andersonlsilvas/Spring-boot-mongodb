5214eee9c2c1a56c834590b89204ae6927319762
Anderson Santos <andersonlsilvas86@gmail.com>
2024-03-16 20:02:34 -0300

    Criando entity Post com User aninhado

---
+++ springboot_mongodb/src/main/java/com/andersonsantos/springboot_mongodb/Repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.andersonsantos.springboot_mongodb.Repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.andersonsantos.springboot_mongodb.entities.Post;
+
+@Repository
+public interface PostRepository extends	MongoRepository<Post, String>{
+	
+}
--- springboot_mongodb/src/main/java/com/andersonsantos/springboot_mongodb/config/Instantiation.java
+++ springboot_mongodb/src/main/java/com/andersonsantos/springboot_mongodb/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.andersonsantos.springboot_mongodb.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.andersonsantos.springboot_mongodb.Repository.PostRepository;
 import com.andersonsantos.springboot_mongodb.Repository.UserRepository;
+import com.andersonsantos.springboot_mongodb.entities.Post;
 import com.andersonsantos.springboot_mongodb.entities.User;
 
 @Configuration
@@ -14,18 +18,29 @@ public class Instantiation implements CommandLineRunner{
 	
 	@Autowired
 	public UserRepository userRepository;
+	
+	@Autowired
+	public PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		Post post1 = new Post(null, sdf.parse("16/03/2024"), "Partiu Viagem", "Vou viajar para São Paulo. Abraçõs!", maria);
+		Post post2 = new Post(null, sdf.parse("18/03/2024"), "Bom dia", "Acordei feliz hoje!", maria);
 		
+				
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1 ,post2));
 	}
 
 	
