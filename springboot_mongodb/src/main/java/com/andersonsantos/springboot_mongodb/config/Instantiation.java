ffced15343be04b33af9359f991d6aa2d0322919
Anderson Santos <andersonlsilvas86@gmail.com>
2024-03-16 14:50:18 -0300

    Operação de instanciação da base de dados

---
+++ springboot_mongodb/src/main/java/com/andersonsantos/springboot_mongodb/config/Instantiation.java
@@ -0,0 +1,34 @@
+package com.andersonsantos.springboot_mongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.andersonsantos.springboot_mongodb.Repository.UserRepository;
+import com.andersonsantos.springboot_mongodb.entities.User;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+	
+	@Autowired
+	public UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+
+	
+	
+	
+}
